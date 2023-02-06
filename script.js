570cc1897ffb9915c04c368b0bbc837693b283d0
EL_LEONEL <104507488+elleonel@users.noreply.github.com>
2023-02-06 17:57:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
+/** 
+ * Disable right-click of mouse, F12 key, and save key combinations on page 
+ */ 
+ document.addEventListener("contextmenu", function(e){ 
+ e.preventDefault(); 
+ }, false); 
+ document.addEventListener("keydown", function(e) { 
+ //document.onkeydown = function(e) { 
+ // "I" key 
+ if (e.ctrlKey && e.shiftKey && e.keyCode == 73) { 
+ disabledEvent(e); 
+ } 
+ // "J" key 
+ if (e.ctrlKey && e.shiftKey && e.keyCode == 74) { 
+ disabledEvent(e); 
+ } 
+ // "S" key + macOS 
+ if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) { 
+ disabledEvent(e); 
+ } 
+ // "U" key 
+ if (e.ctrlKey && e.keyCode == 85) { 
+ disabledEvent(e); 
+ } 
+ // "F12" key 
+ if (event.keyCode == 123) { 
+ disabledEvent(e); 
+ } 
+ // "C" key 
+ if (e.ctrlKey && event.keyCode == 67) { 
+ disabledEvent(e); 
+ } 
+ }, false); 
+ function disabledEvent(e){ 
+ if (e.stopPropagation){ 
+ e.stopPropagation(); 
+ } else if (window.event){ 
+ window.event.cancelBubble = true; 
+ } 
+ e.preventDefault(); 
+ return false; 
+ }
